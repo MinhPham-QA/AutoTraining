ca7d52c62bd57f38f43fb4cca6b68d37f53adf3c
MinhPham-QA <77136288+MinhPham-QA@users.noreply.github.com>
2021-07-14 23:49:11 +0700

    update test.java

--- new folder/test.java
+++ new folder/test.java
@@ -1,3 +1,5 @@
-well, I'll make some changes here
-let's see if this really affects
-this is just a test file, please be caution
+This is java file in new folder
+I updated all the content inside the java file
+Let's see if this really affects
+  well, I'll do it
+  more sentences for stronger edit
